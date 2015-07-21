c8134f5b37a643dc2b9e110bdca034418c66c0a1
seaguest <tyhk1987@gmail.com>
2015-07-21 14:12:16 +0000

    new commit

---
+++ app/src/main/java/com/seaguest/game/chessboard/Chessman.java
@@ -0,0 +1,15 @@
+
+package com.seaguest.game.chessboard;
+
+public class Chessman extends Point{
+    int color;
+    
+    
+    public Chessman(int x, int y, int chessColor) {
+		super(x, y);
+		color = chessColor;
+	}
+
+
+    
+}    
---
+++ app/src/main/java/com/seaguest/game/chessboard/Point.java
@@ -0,0 +1,25 @@
+package com.seaguest.game.chessboard;
+
+// for coordiante
+public class Point{
+    
+    int X;
+    int y;
+    
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    
+	@Override
+	public boolean equals(Object o) {
+        boolean equals = false;
+        if(o instanceof Point){
+        	Point anotherPoint = (Point)o;
+        	equals = (this.x==anotherPoint.x && this.y==anotherPoint.y);
+        }
+		return equals;
+	}
+
+}
